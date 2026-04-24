3a5ae85daeb360483f3263835658088f27a00033
akspriya200408@gmail.com <akspriya200408@gmail.com>
2026-04-24 14:31:34 +0530

    [Akshaya Priya]:Added:UC14 Spring Batch structure

---
+++ src/main/java/com/fundoonotes/batch/BatchConfig.java
@@ -0,0 +1,7 @@
+package com.fundoonotes.batch;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class BatchConfig {
+}
---
+++ src/main/java/com/fundoonotes/batch/NoteImportRow.java
@@ -0,0 +1,22 @@
+package com.fundoonotes.batch;
+
+public class NoteImportRow {
+    private String title;
+    private String description;
+
+    public NoteImportRow() {
+    }
+
+    public NoteImportRow(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
